0c0516a176de409de1f194d4acaf3d6d22d5cf08
Francesco <f.demicco.01@gmail.com>
2025-06-20 00:48:29 +0200

    typing effect

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Handicon from './icons/Handicon.jsx'
 import Sendicon from './icons/Sendicon.jsx'
+import Typed from 'typed.js';
 
 const Data = () => {
+  const typedRef = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(typedRef.current, {
+      strings: ["Web Developer", "Frontend Developer", "Backend Developer", "Database expert"],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 2000
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+
   return (
     <div className="home__data">
         <h1 className="home__title">
           Francesco <Handicon></Handicon>
         </h1>
-        <h3 className="home__subtitle"><span className="typedText">Web Developer</span></h3>
+        <h3 className="home__subtitle"><span className="typedText" ref={typedRef}>Web Developer</span></h3>
         <p className='home__description'>
-            I'm a web developer based in Naples. 22 yeras old.
+            I'm a web developer based in Naples. 22 yeras old with over 3 years of experience.
         </p>
 
         <a href="#contact" className="button button--flex">

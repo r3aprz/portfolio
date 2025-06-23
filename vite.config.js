59c175d51dc2b610a3c3238ea3bd87531129247b
Francesco <f.demicco.01@gmail.com>
2025-06-23 21:52:32 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  server: {
-    allowedHosts: ['gelding-needed-equally.ngrok-free.app'] // '.ngrok-free.app' oppure all
-  }
-})
+  ...(command === 'serve' && {
+    server: {
+      allowedHosts: ['gelding-needed-equally.ngrok-free.app']
+    }
+  })
+}))
